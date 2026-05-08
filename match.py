dc5122696edfea3b0086c95fe8d52c7462dc55b5
SAYAN <spal75084@gmail.com>
2026-05-08 21:08:27 +0530

    Update match.py

--- match.py
+++ match.py
@@ -7,7 +7,7 @@ import re
 import cloudscraper
 
 # --- Configuration ---
-CRICHD_BASE_URL = "https://vf.crichd.tv/"
+CRICHD_BASE_URL = "https://crichd.top/"
 OUTPUT_JSON_FILE = "matches.json"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 REQUESTS_TIMEOUT = 10
